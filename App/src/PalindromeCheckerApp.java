8562615e27def96c98b4528e0413a2c7684ce067
pragnyamallavarapu0 <pragnyamallavarapu07@gmail.com>
2026-02-27 15:50:21 +0530

    UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 /**
  * Application entry point
@@ -8,8 +10,26 @@ public class PalindromeCheckerApp {
  * @Version 1.0
  */
    public static void main(String[]args){
-       System.out.println("Welcome to the Palindrome Checker Management System");
-       System.out.println("Version: 1.0");
-       System.out.println("System intialized Successfully.");
+       Scanner sc =new Scanner(System.in);
+       System.out.println("Input text :");
+       String input =sc.nextLine();
+       boolean isPalindrome;
+       for (int i =0; i < input.length()/2; i++){
+
+           if (input.charAt(i) != input.charAt(input.length() -1 -i)){
+               isPalindrome =false;
+               break;
+           }
+           else{
+               isPalindrome =true;
+               break;
+           }
+       }
+       if(isPalindrome =true){
+             System.out.println("is it a palindrome? :true");
+       }
+       else{
+             System.out.println("s it a palindrome? :false");
+       }
    }
 }
